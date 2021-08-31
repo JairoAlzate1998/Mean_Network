48cd14632f1ad4109f321d33b06a86daa8a8e541
Jairo Alzate <jairo.alzate@siesa.com>
2021-08-31 10:28:40 -0500

    Add registerUser front

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,25 +11,45 @@ import { RegisterComponent } from './home/register/register.component';
 import { FooterComponent } from './home/footer/footer.component';
 import { SavePostComponent } from './board/save-post/save-post.component';
 import { ListPostComponent } from './board/list-post/list-post.component';
-import { BoardService } from "./services/board.service";
-import { UserService } from "./services/user.service";
-import { TokenInterceptorService } from "./services/token-interceptor.service";
-import { AuthGuard } from "./guard/auth.guard";
+import { BoardService } from './services/board.service';
+import { UserService } from './services/user.service';
+import { TokenInterceptorService } from './services/token-interceptor.service';
+import { AuthGuard } from './guard/auth.guard';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, LoginComponent, RegisterComponent, FooterComponent, SavePostComponent, ListPostComponent],
-  imports: [BrowserModule,AppRoutingModule,BrowserAnimationsModule,HttpClientModule,FormsModule,ReactiveFormsModule,MatToolbarModule,MatButtonModule],
-  providers: [
-    BoardService,
-    UserService,
-    TokenInterceptorService,
-    AuthGuard
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    FooterComponent,
+    SavePostComponent,
+    ListPostComponent,
   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+    MatSnackBarModule
+  ],
+  providers: [BoardService, UserService, TokenInterceptorService, AuthGuard],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
